3c3bca61d9fefaf1da4731383ab8d50da05c7d88
Soikat <193371447+soikat27@users.noreply.github.com>
2025-09-25 19:59:24 -0400

    Initialize and print empty TicTacToe board
    -- Add board array
    -- Implement printBoard()

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,19 @@
 public class TicTacToe {
     public static void main(String[] args)
 	{
-        
+        char[][] board = {{' ', ' ', ' '}, 
+				    	  {' ', ' ', ' '}, 
+				    	  {' ', ' ', ' '}};
+		
+		printBoard(board);
 	}
+
+	private static void printBoard(char[][] board)
+	{
+        System.out.println (board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+		System.out.println ("-+-+-");
+		System.out.println (board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+		System.out.println ("-+-+-");
+		System.out.println (board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+    }
 }
