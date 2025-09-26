a6e362c3eedfa1e95618ee42568326d2b9eebfa1
Soikat <193371447+soikat27@users.noreply.github.com>
2025-09-25 20:11:18 -0400

    Add move placement & input validation facility
    -- Implement placeMove() to place the move in the board
    -- Implement isValidMove() to check for input validation

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,16 @@
+/**
+ * A simple console-based Tic-Tac-Toe game.
+ * Supports a human player vs a computer opponent.
+ */
 public class TicTacToe {
-    public static void main(String[] args)
+    
+	/**
+     * Entry point for the Tic-Tac-Toe game.
+     * Initializes the board and alternates moves between player and computer.
+     *
+     * @param args Command-line arguments (not used)
+     */
+	public static void main(String[] args)
 	{
         char[][] board = {{' ', ' ', ' '}, 
 				    	  {' ', ' ', ' '}, 
@@ -8,6 +19,11 @@ public class TicTacToe {
 		printBoard(board);
 	}
 
+	/**
+     * Prints the current Tic-Tac-Toe board to the console.
+     *
+     * @param board 3x3 character array representing the board
+     */
 	private static void printBoard(char[][] board)
 	{
         System.out.println (board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
@@ -16,4 +32,81 @@ public class TicTacToe {
 		System.out.println ("-+-+-");
 		System.out.println (board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
     }
+
+	/**
+     * Places a move on the board for a given symbol at a user-specified position.
+     *
+     * @param board     3x3 character array representing the board
+     * @param userInput The position (1-9) where the player wants to move
+     * @param symbol    'x' for player or 'o' for computer
+     */
+	private static void placeMove(char[][] board, String userInput, char symbol) 
+	{
+		switch (userInput) 
+		{
+			case "1":
+				board[0][0] = symbol;
+				break;
+			case "2":
+				board[0][1] = symbol;
+				break;
+			case "3":
+				board[0][2] = symbol;
+				break;
+			case "4":
+				board[1][0] = symbol;
+				break;
+			case "5":
+				board[1][1] = symbol;
+				break;
+			case "6":
+				board[1][2] = symbol;
+				break;
+			case "7":
+				board[2][0] = symbol;
+				break;
+			case "8":
+				board[2][1] = symbol;
+				break;
+			case "9":
+				board[2][2] = symbol;
+				break;
+			default:
+				break;
+		}
+	}
+
+	/**
+     * Checks if a given move is valid (the cell is empty).
+     *
+     * @param board     3x3 character array representing the board
+     * @param userInput The position (1-9) to check
+     * @return true if the cell is empty, false otherwise
+     */
+	private static boolean isValidMove(char[][] board, String userInput) 
+	{
+		switch (userInput) 
+		{
+			case "1":
+				return board[0][0] == ' ';
+			case "2":
+				return board[0][1] == ' ';
+			case "3":
+				return board[0][2] == ' ';
+			case "4":
+				return board[1][0] == ' ';
+			case "5":
+				return board[1][1] == ' ';
+			case "6":
+				return board[1][2] == ' ';
+			case "7":
+				return board[2][0] == ' ';
+			case "8":
+				return board[2][1] == ' ';
+			case "9":
+				return board[2][2] == ' ';
+			default:
+				return false;
+		}
+	}
 }
